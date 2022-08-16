a8072a2ecb50f63a7bd81e7ebbf0d40f626526ee
laurence-martin <laurence_martin@outlook.com>
2022-08-15 21:10:22 -0300

    Inciando a publicação v4

--- MoneyExtension.Tests/MoneyExtensionTests.cs
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -6,8 +6,8 @@ public class MoneyExtensionTests
     [TestMethod]
     public void ShouldConvertDecimalToInt()
     {
-        decimal valor = 279.98M;
+        decimal valor = 279.97M;
         var cents = valor.ToCents();
-        Assert.AreEqual(27998, cents);
+        Assert.AreEqual(27997, cents);
     }
 }
